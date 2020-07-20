c6dee6d51a599f74162143be7cf935cf5412cbe2
Agustín Díaz <me@adiaz.uy>
2020-07-20 16:56:26 +0200

    Use storybook as test environment

---
+++ stories/index.stories.js
@@ -0,0 +1,7 @@
+import '../knopf.css';
+
+export default { title: 'Knopf' };
+
+export const Knopf = () => '<button class="knopf">Knopf</button>';
+
+export const Flat = () => '<button class="knopf flat">Flat</button>';
